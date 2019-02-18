dbc7af882aec5eb01782be8c27f375da6386bd02
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-02-18 11:27:27 +0000

    new navigtion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,67 +1,116 @@
-import React from 'react';
-import cx from 'classnames';
-import '../styles/components/container.scss';
-import '../styles/components/header.scss';
+import React from "react";
+import cx from "classnames";
+import "../styles/components/container.scss";
+import "../styles/components/header.scss";
+
+import mobiscroll from "@mobiscroll/react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
 
 class Header extends React.Component {
-    constructor() {
-        super();
-        this.state = {isOpen: false};
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  handleChange() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    render() {
-        const navListClasses = cx('navigation__main__list', {
-            'navigation__main__list--open': this.state.isOpen
-        });
+  render() {
+    const navListClasses = cx("navigation__main__list", {
+      "navigation__main__list--open": this.state.isOpen
+    });
 
-        return (
-            <nav className="navigation ">
-                <div className="navigation__main container">
-                    <h3>parcel <span></span> mario</h3>
-                    <div className="navigation__main__toggle" >
-                        <div className='tgg'></div>
-                        <div className='tgg'></div>
-                        <div className='tgg'></div>
-                        <p>MENU</p>
-                    </div>
-                    <ul className={navListClasses}>
-                        <li>
-                            <a className="sendAParcel" href="#1">Send a Parcel</a>
-                        </li>
-                        <li>
-                            <a className="" href="#2">Tracking</a>
-                        </li>
-                        <li>
-                            <a className="" href="#3">Service</a>
-                        </li>
-                        <li>
-                            <a className="" href="#4">About Us</a>
-                        </li>
-                        <li>
-                            <a className="" href="#5">Contact Us</a>
-                        </li>
-                        <li>
-                            <a className="" href="#6">Items in cart: 0</a>
-                        </li>
-                        <li>
-                            <a className="" href="#7">Sing up</a>
-                        </li>
-                        <li>
-                            <a className="" href="#8">Log in</a>
-                        </li>
-                    </ul>
+    return (
+      <nav className="navigation ">
+        <div className="navigation__main container">
+          <h3>
+            parcel <span /> mario
+          </h3>
+          <mobiscroll.BottomNav
+            lang="en-UK"
+            theme="auto"
+            type="hamburger"
+            display="inline"
+          >
+            <mobiscroll.NavItem icon="newspaper">
+              Send a Parcel
+            </mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="material-people">
+              Tracking
+            </mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="bubble">Service</mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="fa-globe">Contact Us</mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="camera" badge="1">
+              Items in cart
+            </mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="calendar">Sing up</mobiscroll.NavItem>
+            <mobiscroll.NavItem icon="line-settings">Log in</mobiscroll.NavItem>
+          </mobiscroll.BottomNav>
+          {/* <div className="md-hamb ">
+            <mobiscroll.HamburgerNav lang="en-UK" theme="ios" type="hamburger">
+              <mobiscroll.NavItem icon="newspaper">
+                Send a Parcel
+              </mobiscroll.NavItem>
+              <mobiscroll.NavItem icon="material-people">
+                Tracking
+              </mobiscroll.NavItem>
+              <mobiscroll.NavItem icon="bubble">Messages</mobiscroll.NavItem>
+              <mobiscroll.NavItem icon="fa-globe">
+                Notifications
+              </mobiscroll.NavItem>
+            </mobiscroll.HamburgerNav>
+          </div> */}
 
-                </div>
-            </nav>
-        );
-    }
+          {/* <ul className={navListClasses}>
+            <li>
+              <a className="sendAParcel" href="#1">
+                Send a Parcel
+              </a>
+            </li>
+            <li>
+              <a className="" href="#2">
+                Tracking
+              </a>
+            </li>
+            <li>
+              <a className="" href="#3">
+                Service
+              </a>
+            </li>
+            <li>
+              <a className="" href="#4">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a className="" href="#5">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a className="" href="#6">
+                Items in cart: 0
+              </a>
+            </li>
+            <li>
+              <a className="" href="#7">
+                Sing up
+              </a>
+            </li>
+            <li>
+              <a className="" href="#8">
+                Log in
+              </a>
+            </li>
+          </ul> */}
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
